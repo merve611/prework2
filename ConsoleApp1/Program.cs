9931e866d4fdb6f667361d8e3eba9becd6eeaa45
merve cakiroglu <mrveeckrrr@hotmail.com>
2024-07-16 14:27:17 +0300

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Xml;
+
+namespace HelloWorld
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+            Console.WriteLine("Merve Akkoyunlu !");
+            Console.WriteLine("Merhaba, Patikanın geniş iş ağı sayesinde işe girebileceğimi düşündüğüm için bu bootcampteyim :)");
+        } 
+	}
+}
